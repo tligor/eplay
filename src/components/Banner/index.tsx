5626d729a09d6bc131d2cdcc546a7f608198036a
Igor Santana <santanna.ig@hotmail.com>
2025-02-02 03:30:21 -0300

    correções no visual do banner, implementação da tag e botão, uso do react-router-dom

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,15 +1,27 @@
 import { Imagem, Precos, Titulo } from './styles'
 import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
 
 const Banner = () => (
   <Imagem style={{ backgroundImage: `url(${bannerImg})` }}>
     <div className="container">
-      <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
-      <Precos>
-        De <span>R$ 250,00</span>
-        <br />
-        Por apenas R$ 99,90
-      </Precos>
+      <Tag size="big">Destaque</Tag>
+      <div>
+        <Titulo>Marvel&apos;s Spider-Man: Miles Morales PS4 & PS5</Titulo>
+        <Precos>
+          De <span>R$ 250,00</span>
+          <br />
+          Por apenas R$ 99,90
+        </Precos>
+      </div>
+      <Button
+        type="link"
+        to="/produto"
+        title="Clique aqui para aproveitar a oferta"
+      >
+        Aproveitar
+      </Button>
     </div>
   </Imagem>
 )
