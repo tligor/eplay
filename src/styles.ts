638d201f4d7812e612efa8c1735889e154ca0d7d
Igor Santana <santanna.ig@hotmail.com>
2025-02-02 01:37:02 -0300

    realizada implementação do cabecalho

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const cores = {
+export const cores = {
   branco: '#EEE',
   preto: '#111',
   cinza: '#333',
@@ -13,9 +13,17 @@ export const GlobalCss = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: 'Roboto Mono', sans-serif;
+    list-style: none;
   }
   body{
     background-color: ${cores.preto};
-    color: ${cores.branco}
+    color: ${cores.branco};
+    padding-top: 40px;
   }
   `
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  margin-bottom: 80px;
+`
