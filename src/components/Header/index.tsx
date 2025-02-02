3eea6e5c27d73b1a3091c48b4b8ab0436a4229b4
Igor Santana <santanna.ig@hotmail.com>
2025-02-02 06:11:16 -0300

    Criando footer, adicionando acesso rapido, modificações no header logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,9 @@ import carrinho from '../../assets/images/carrinho.svg'
 const Header = () => (
   <HeaderBar>
     <div>
-      <img src={logo} alt="EPLAY" />
+      <Link to="/">
+        <img src={logo} alt="EPLAY" />
+      </Link>
       <nav>
         <Links>
           <LinkItem>
